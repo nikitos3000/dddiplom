16a7d493fed331a951ed3dc21df6dd011e9c0083
Your Name <trinevnikita@gmail.com>
2024-12-25 15:28:51 +0300

    fix focus

--- modules/MyLabel.py
+++ modules/MyLabel.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtWidgets
 
+
 class MyLabel(QtWidgets.QLabel):
     colorYellow = "#FFFF90"
     colorOrange = "#F5D8C1"
@@ -9,10 +10,12 @@ class MyLabel(QtWidgets.QLabel):
 
     changeCellFocus = QtCore.pyqtSignal(int)
 
+    currentFocusedCell = None  # Статическая переменная для отслеживания текущей фокусированной клетки
+
     def __init__(self, id, bgColor, parent=None):
-        QtWidgets.QLabel.__init__(self, parent)
+        super().__init__(parent)
         self.setAlignment(QtCore.Qt.AlignCenter)
-        self.setFixedSize(30, 30)
+        self.setFixedSize(45, 60)
         self.setMargin(0)
         self.setText("")
         if id < 0 or id > 80:
@@ -25,20 +28,29 @@ class MyLabel(QtWidgets.QLabel):
         self.showColorCurrent()
 
     def mousePressEvent(self, evt):
+        # Сбрасываем фокус с текущей клетки, если она есть
+        if MyLabel.currentFocusedCell is not None:
+            MyLabel.currentFocusedCell.clearCellFocus()
+
+        # Устанавливаем фокус на текущую клетку
+        MyLabel.currentFocusedCell = self
+        self.setCellFocus()
+
+        # Излучаем сигнал
         self.changeCellFocus.emit(self.id)
-        QtWidgets.QLabel.mousePressEvent(self, evt)
+        super().mousePressEvent(evt)
 
     def showColorCurrent(self):
         self.setStyleSheet(
-        f"background-color: {self.bgColorCurrent}; color: {self.fontColorCurrent};"
-    )
+            f"background-color: {self.bgColorCurrent}; color: {self.fontColorCurrent};"
+        )
 
     def setCellFocus(self):
         self.bgColorCurrent = self.colorYellow
         self.showColorCurrent()
 
     def clearCellFocus(self):
-        self.fontColorCurrent = self.bgColorDefault
+        self.bgColorCurrent = self.bgColorDefault
         self.showColorCurrent()
 
     def setNewText(self, text):
